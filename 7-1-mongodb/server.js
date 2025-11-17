340f9d7a466123d9719249c6abbfe62cbdb99d5d
MoayedAljaddawi24 <s202248800@kfupm.edu.sa>
2025-11-17 19:53:36 +0300

    task 3

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -196,6 +196,15 @@ const Student = mongoose.model("Student", studentSchema);
 
 
 // create document
+async function createStudents() {
+  await Student.insertMany([
+    { name: "Ali", age: 21, major: "CS" },
+    { name: "Sara", age: 23, major: "SE" },
+  ]);
+  console.log("🤘. Inserted");
+}
+
+createStudents();
 
 
 // read document
